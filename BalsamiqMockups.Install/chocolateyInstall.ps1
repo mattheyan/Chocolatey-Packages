b176f7e597d7654d36b1ed0cc6ddbc2d6b081e72
Bryan Matthews <mattheyan@gmail.com>
2014-05-19 18:41:38 -0400

    Ensure that balsamiq install runs with elevated permissions. Add file type registration and desktop link. Implement uninstall.

--- BalsamiqMockups.Install/chocolateyInstall.ps1
+++ BalsamiqMockups.Install/chocolateyInstall.ps1
@@ -1,16 +1,67 @@
 try {
+	# Create variable for program files directory
+	# ===========================================
+	# Borrowed from BoxStarter.Azure
+	if(${env:ProgramFiles(x86)} -ne $null) {
+		$programFiles86 = ${env:ProgramFiles(x86)}
+	} else {
+		$programFiles86 = $env:ProgramFiles
+	}
+
 	# Instructions for silent installation: http://support.balsamiq.com/customer/portal/articles/133390
-	$installPath = Join-Path ${env:ProgramFiles(x86)} "Balsamiq Mockups"
+	$installPath = Join-Path $programFiles86 "Balsamiq Mockups"
 
 	# Download and extract ZIP file
 	$tempDir = [System.IO.Path]::GetTempFileName().Replace(".", "")
 	$zipUrl = "http://builds.balsamiq.com/b/mockups-desktop/MockupsForDesktop.zip"
+	Write-Host "Downloading zip file..."
 	Install-ChocolateyZipPackage "balsamiqmockups" $zipUrl $tempDir
 
 	# Move child folder into target folder
-	Move-Item (Join-Path $tempDir "MockupsForDesktop") $installPath
-	
-	# TODO: Add file type registration
+	Write-Host "Moving application files..."
+	$sourcePath = Join-Path $tempDir 'MockupsForDesktop'	
+	$elevatedMoveFiles = "`
+	Move-Item '$sourcePath' '$installPath';`
+	return 0;"
+	Start-ChocolateyProcessAsAdmin $elevatedMoveFiles
+
+	# Add file type registration
+	Write-Host "Adding file type registration..."
+	$balsamiqExe = Join-Path $installPath "Balsamiq Mockups.exe"
+	$elevatedSetFileAssociation = "`
+    if( -not (Test-Path -path HKCR:) ) {New-PSDrive -Name HKCR -PSProvider registry -Root Hkey_Classes_Root};`
+    if(!(test-path -LiteralPath 'HKCR:\.bmml')) { new-item -Path 'HKCR:\.bmml' };`
+    Set-ItemProperty -LiteralPath 'HKCR:\.bmml' -Name '(Default)'  -Value 'com.balsamiq.mockupfile';`
+    Set-ItemProperty -LiteralPath 'HKCR:\.bmml' -Name 'Content Type'  -Value 'application/xml';`
+    if(!(test-path -LiteralPath 'HKCR:\.bmml\OpenWithProgIds')) { new-item -Path 'HKCR:\.bmml\OpenWithProgIds' };`
+    Set-ItemProperty -LiteralPath 'HKCR:\.bmml\OpenWithProgIds' -Name 'com.balsamiq.mockupfile' -Value '';`
+    if(!(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile')) { new-item -Path 'HKCR:\com.balsamiq.mockupfile' };`
+    Set-ItemProperty -LiteralPath 'HKCR:\com.balsamiq.mockupfile' -Name '(Default)' -Value 'Balsamiq Mockups Markup Language';`
+    if(!(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile\DefaultIcon')) { new-item -Path 'HKCR:\com.balsamiq.mockupfile\DefaultIcon' };`
+    Set-ItemProperty -LiteralPath 'HKCR:\com.balsamiq.mockupfile\DefaultIcon' -Name '(Default)' -Value '\""$balsamiqExe\"",-105';`
+    if(!(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile\shell')) { new-item -Path 'HKCR:\com.balsamiq.mockupfile\shell' };`
+    if(!(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile\shell\open')) { new-item -Path 'HKCR:\com.balsamiq.mockupfile\shell\open' };`
+    if(!(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile\shell\open\command')) { new-item -Path 'HKCR:\com.balsamiq.mockupfile\shell\open\command' };`
+    Set-ItemProperty -LiteralPath 'HKCR:\com.balsamiq.mockupfile\shell\open\command' -Name '(Default)' -Value '\""$balsamiqExe\"" \""%1\""';`
+    return 0;"
+	Start-ChocolateyProcessAsAdmin $elevatedSetFileAssociation
+
+	# Create desktop shortcut
+	$wshShell = New-Object -COMObject WScript.Shell
+	$currentUser = (Get-WMIObject -class Win32_ComputerSystem | select username).username
+	if ($currentUser -match "\\") {
+		$currentUser = $currentUser.Substring($currentUser.IndexOf("\") + 1)
+	}
+	$usersDir = Split-Path $env:USERPROFILE -Parent
+	$currentUserDir = Join-Path $usersDir $currentUser
+	$currentUserDesktopDir = Join-Path $currentUserDir "Desktop"
+	$desktopLinkPath = Join-Path $currentUserDesktopDir "Balsamiq Mockups.lnk"
+	if (!(Test-Path $desktopLinkPath)) {
+		Write-Host "Creating Desktop shortcut..."
+		$desktopShortcut = $wshShell.CreateShortcut($desktopLinkPath)
+		$desktopShortcut.TargetPath = $balsamiqExe
+		$desktopShortcut.Save()
+	}
 
 	Write-ChocolateySuccess "balsamiqmockups.install"
 } catch {
---
+++ BalsamiqMockups.Install/chocolateyUninstall.ps1
@@ -0,0 +1,45 @@
+try {
+	# Instructions for silent installation: http://support.balsamiq.com/customer/portal/articles/133390
+	$installPath = Join-Path ${env:ProgramFiles(x86)} "Balsamiq Mockups"
+
+	# Delete desktop shortcut
+	$currentUser = (Get-WMIObject -class Win32_ComputerSystem | select username).username
+	if ($currentUser -match "\\") {
+		$currentUser = $currentUser.Substring($currentUser.IndexOf("\") + 1)
+	}
+	$usersDir = Split-Path $env:USERPROFILE -Parent
+	$currentUserDir = Join-Path $usersDir $currentUser
+	$currentUserDesktopDir = Join-Path $currentUserDir "Desktop"
+	$desktopLinkPath = Join-Path $currentUserDesktopDir "Balsamiq Mockups.lnk"
+	if (Test-Path $desktopLinkPath) {
+		Write-Host "Deleting Desktop shortcut..."
+		$elevatedRemoveShortcut = "`
+		Remove-Item '$desktopLinkPath' -Force;`
+		return 0;"
+		Start-ChocolateyProcessAsAdmin $elevatedRemoveShortcut
+	}
+
+	# Remove file type registration
+	Write-Host "Removing file type registration..."
+	$balsamiqExe = Join-Path $installPath "Balsamiq Mockups.exe"
+	$elevatedRemoveFileAssociation = "`
+    if( -not (Test-Path -path HKCR:) ) {New-PSDrive -Name HKCR -PSProvider registry -Root Hkey_Classes_Root};`
+    if(test-path -LiteralPath 'HKCR:\.bmml') { remove-item -Path 'HKCR:\.bmml' -Recurse };`
+    if(test-path -LiteralPath 'HKCR:\com.balsamiq.mockupfile') { remove-item -Path 'HKCR:\com.balsamiq.mockupfile' -Recurse };`
+    return 0;"
+	Start-ChocolateyProcessAsAdmin $elevatedRemoveFileAssociation
+
+	# Delete application files
+	if (Test-Path $installPath) {
+		Write-Host "Deleting application files..."
+		$elevatedRemoveFiles = "`
+		Remove-Item '$installPath' -Force -Recurse;`
+		return 0;"
+		Start-ChocolateyProcessAsAdmin $elevatedRemoveFiles
+	}
+
+	Write-ChocolateySuccess "balsamiqmockups.install"
+} catch {
+    Write-ChocolateyFailure "balsamiqmockups.install" $($_.Exception.Message)
+    throw
+}
