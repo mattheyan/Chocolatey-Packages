e016459a3cce2101510a6ba965fa9afd626f31dc
Bryan Matthews <mattheyan@gmail.com>
2015-07-14 09:54:51 -0400

    Add SysInternals 'Desktops' package.

---
+++ Desktops/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$installTargetPath = Join-Path $env:APPDATA 'SysInternals\Desktops'
+New-Item -Path $installTargetPath -Type Directory -Force | Out-Null
+
+Install-ChocolateyZipPackage 'Desktops' 'http://download.sysinternals.com/files/Desktops.zip' $installTargetPath
+
+$exePath = Join-Path $installTargetPath 'Desktops.exe'
+
+Install-ChocolateyDesktopLink $exePath
+
+try {
+    $desktopPath = Join-Path $env:USERPROFILE 'Desktop'
+    $startMenuLocation = Join-Path $env:APPDATA 'Microsoft\Windows\Start Menu'
+    Move-Item (Join-Path $desktopPath 'Desktops.exe.lnk') (Join-Path $startMenuLocation 'Desktops.lnk')
+}
+catch{
+    Write-Output "WARNING - Unable to move desktop shortcut: $_.Exception.Message"
+}
+
+Start-Process $exePath
